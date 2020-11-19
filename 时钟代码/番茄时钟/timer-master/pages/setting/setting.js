7d577546d58f0203cea61636d64e087dc401ed0e
PCWJSW <70789836+PCWJSW@users.noreply.github.com>
2020-11-19 21:59:32 +0800

    Add files via upload

---
+++ 时钟代码/番茄时钟/timer-master/pages/setting/setting.js
@@ -0,0 +1,17 @@
+Page({
+  onShow: function() {
+    wx.setNavigationBarTitle({
+      title: '设置'
+    })
+    this.setData({
+    	workTime: wx.getStorageSync('workTime'),
+    })
+  },
+  changeWorkTime: function(e) {
+  	wx.setStorage({
+  		key: 'workTime',
+  		data: e.detail.value
+  	})
+  },
+
+})
